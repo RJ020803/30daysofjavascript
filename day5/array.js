fc91e89344a514244c87f217551dc330581a1614
RJ020803 <142171076+RJ020803@users.noreply.github.com>
2024-09-24 20:52:38 +0530

    day4 complted

---
+++ day5/array.js
@@ -0,0 +1,100 @@
+//using array constructor
+const arr=Array()
+console.log(arr);
+
+//
+using square brackets([])
+
+const arr=[]
+console.log(arr);
+
+//how to create an array with values 
+const numbers=[0,3.14,9.81,37,98.6,100]//this is called array of numbers 
+const fruits=['banana','orange','mango','lemon']
+const vegetables=['tomato','potato','cabbage','onion','carrot','capsicum']
+const animalproducts=['milk','butter','youghurt']
+const webTechs=['html','css','js','react','node','mongoDB']
+const countries=['finland','sweden','denmark','norway','iceland']
+
+console.log('Numbers',numbers );
+console.log('numbers of numbers ',numbers.length);
+
+console.log('vegetables',vegetables );
+console.log('vegetables of vegetables ',vegetables.length);
+
+console.log('Numbers',numbers );
+console.log('numbers of numbers ',animalproduct.length);
+
+console.log('Numbers',numbers );
+console.log('numbers of numbers ',.length);
+
+console.log('Numbers',numbers );
+console.log('numbers of numbers ',numbers.length);
+
+
+//array using split
+let js='javascript'
+const charInjavascript=js.split('')
+
+console.log(charInjavascript);
+
+let js = 'JavaScript'
+const charsInJavaScript = js.split('')
+
+console.log(charsInJavaScript) // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+console.log(companies) 
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = txt.split(' ')
+
+console.log(words)
+
+//array have items of different data types
+const arr=[
+  'rimi',
+  566,
+  true,
+  {country:'finland',city:'ara'},
+  {skills:mandhak ko sudharna }
+]
+
+
+//accesing array items using index
+
+const mandhak=['tadpole','gorela','cat']
+let firstmandhak=mandhak[0]
+
+console.log(firstmandhak);
+
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100]  // set of numbers
+
+console.log(numbers.length)  81, 37, 98.6, 100]
+console.log(numbers[0])      
+console.log(numbers[5])      
+
+let lastIndex = numbers.length - 1;
+console.log(numbers[lastIndex]) 
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+] 
+
+console.log(webTechs)        // all the array items
+console.log(webTechs.length) 
+console.log(webTechs[0])     
+console.log(webTechs[6])     
+
+let lastIndex = webTechs.length - 1
+console.log(webTechs[lastIndex])
+
+///modify an arrya element
+
